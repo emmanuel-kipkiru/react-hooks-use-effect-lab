2b1cc285fb03a9b5cef004f106fa0b21d73c10ad
emmanuel-kipkiru <emmanuelkipkirui17@gmail.com>
2022-07-23 23:37:13 +0300

    use-effect-lab-assign-done

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  useEffect(() => {
+    const time = setTimeout(() => {
+      setTimeRemaining((timeRemaining) => (timeRemaining -= 1));
+    }, 1000);
+
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+      // return;
+    }
+
+    return function () {
+      clearTimeout(time);
+    };
+  }, [timeRemaining, onAnswered]);
+
+  function handleAnswer(isCorrect) {
+    setTimeRemaining(10);
+    onAnswered(isCorrect);
+  }
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
